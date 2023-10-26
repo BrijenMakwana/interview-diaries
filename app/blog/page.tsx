7b7c9632272cadeee67c0ac09d5bfdce30a3e413
Brijen Makwana <brijenma@gmail.com>
2023-10-26 11:44:47 +0530

    SearchBar

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import InterviewCard from "@/components/interview-card";
+import SearchBar from "@/components/search-bar";
 import { db, collection, getDocs } from "@/firebase/firebase";
 import { useEffect, useState } from "react";
 
 export default function BlogPage() {
-  const [articles, setArticles] = useState<any>([]);
+  const [articles, setArticles] = useState<any[]>([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredBlogs, setFilteredBlogs] = useState<any[]>([]);
 
   const getAllArticles = async () => {
     const articlesArray: any[] = [];
@@ -21,18 +24,42 @@ export default function BlogPage() {
 
     setArticles(articlesArray);
 
-    console.log(articlesArray);
+    setFilteredBlogs(articlesArray);
+  };
+
+  const filterBlogs = () => {
+    const filtered = articles.filter((article: any) =>
+      article.company.toLowerCase().includes(searchInput.toLowerCase())
+    );
+
+    setFilteredBlogs(filtered);
   };
 
   useEffect(() => {
     getAllArticles();
   }, []);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      filterBlogs();
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [searchInput]);
+
   return (
-    <section className="flex gap-5 flex-wrap items-center justify-center w-full">
-      {articles.map((item: any) => (
-        <InterviewCard key={item.id} {...item} />
-      ))}
-    </section>
+    <main>
+      <SearchBar setValue={setSearchInput} value={searchInput} />
+
+      <section className="flex gap-5 flex-wrap items-center justify-center w-full mt-12">
+        {searchInput
+          ? filteredBlogs.map((item: any) => (
+              <InterviewCard key={item.id} {...item} />
+            ))
+          : articles.map((item: any) => (
+              <InterviewCard key={item.id} {...item} />
+            ))}
+      </section>
+    </main>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -25,27 +25,6 @@ import { Logo } from "@/components/icons";
 import { UserButton } from "@clerk/nextjs";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -91,7 +70,6 @@ export const Navbar = () => {
             GitHub
           </Button>
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
         <UserButton afterSignOutUrl="/" />
       </NavbarContent>
 
@@ -104,7 +82,6 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
