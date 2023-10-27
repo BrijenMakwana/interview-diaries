cb0d1e8148f3bd16c02faa40624734144ebfe4d0
Brijen Makwana <brijenma@gmail.com>
2023-10-27 16:21:31 +0530

    svg added to home

--- app/page.tsx
+++ app/page.tsx
@@ -7,34 +7,40 @@ import SectionHeading from "@/components/section-heading";
 import CustomDivider from "@/components/custom-divider";
 import NextLink from "next/link";
 import { Button } from "@nextui-org/button";
-import { Input } from "@nextui-org/input";
 import Newsletter from "@/components/newsletter";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center gap-10 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
-        <h1
-          className={title({ color: "yellow" })}
-          style={{
-            textTransform: "capitalize",
-            fontSize: "4rem",
-          }}
-        >
-          interview diaries&nbsp;
-        </h1>
-        <br />
-        <h1 className={title()}>
-          Where Developers Share Their Interview Stories.
-        </h1>
-        <h2 className={subtitle({ class: "mt-4" })}>
-          Explore the World of Developer Interviews.
-        </h2>
-      </div>
+      <section className="flex flex-row items-center justify-evenly w-full flex-wrap gap-10">
+        <Image src="/interview.svg" height={300} width={300} alt="interview" />
 
-      <Button color="primary" as={NextLink} href="/write">
-        Start Writing
-      </Button>
+        <div className="inline-block max-w-lg text-center justify-center">
+          <h1
+            className={title({ color: "yellow" })}
+            style={{
+              textTransform: "capitalize",
+              fontSize: "4rem",
+            }}
+          >
+            interview diaries&nbsp;
+          </h1>
+          <br />
+          <h1 className={title()}>
+            Where Developers Share Their Interview Stories.
+          </h1>
+          <h2 className={subtitle({ class: "mt-4" })}>
+            Explore the World of Developer Interviews.
+          </h2>
+
+          <Button color="primary" as={NextLink} href="/write" className="mt-5">
+            Start Writing
+          </Button>
+        </div>
+      </section>
+
+      <CustomDivider />
 
       <SectionHeading text="featured" highlightedText="interviews" />
       <section className="flex gap-5 flex-wrap items-center justify-evenly w-full">
