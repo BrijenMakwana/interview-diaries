e94ff11b1497ee241bb5250b8d56ea77f6a80c9f
Brijen Makwana <brijenma@gmail.com>
2023-11-01 10:16:28 +0530

    play article

---
+++ components/waveform.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./waveform.css";
+
+const Waveform = () => {
+  return (
+    <div id="bars">
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+    </div>
+  );
+};
+
+export default Waveform;
